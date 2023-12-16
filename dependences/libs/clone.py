c9391f57667e7b09644e18fa1434888da2a21842
kisskaDev <kirill_ii@mail.ru>
2023-12-16 23:39:45 +0100

    update clone.py script

--- dependences/libs/clone.py
+++ dependences/libs/clone.py
@@ -1,32 +1,36 @@
-from os.path import dirname, join
+import os
 import subprocess
-import platform
 
-def clone( baseDir, name, url, commit ):
-    repoDir = join( baseDir, name )
-    print( "========== clone " + name + " ==========" )
-    subprocess.call( ['git', 'clone', url, name], cwd=baseDir )
-    subprocess.call( ['git', 'checkout', commit, '-q'], cwd=repoDir )
+
+class library:
+    def __init__(self, name: str, url: str, commit: str) -> None:
+        self.name = name
+        self.url = url
+        self.commit = commit
+
+    def clone(self, dir: str) -> None:
+        print( "========== clone " + self.name + " ==========" )
+        subprocess.call( ['git', 'clone', self.url, self.name], cwd=dir )
+        subprocess.call( ['git', 'checkout', self.commit, '-q'], cwd=os.path.join(dir, self.name) )
 
 
 if __name__ == '__main__':
-    externalsDir = dirname(__file__)
+    externalsDir = os.path.dirname(__file__)
 
-    clone(externalsDir, 'stb', 'https://github.com/nothings/stb.git', 'af1a5bc352164740c1cc1354942b1c6b72eacb8a')
-    clone(externalsDir, 'tinygltf', 'https://github.com/syoyo/tinygltf.git', 'aaf631c984c6e725573840c193ca2ff0ea216e7b')
-    clone(externalsDir, 'tinyply', 'https://github.com/ddiakopoulos/tinyply.git', 'e5d969413b8612de31bf96604c95bf294d406230')
-    clone(externalsDir, 'vulkan', 'https://github.com/KhronosGroup/Vulkan-Headers.git', '2b55157592bf4c639b76cc16d64acaef565cc4b5')
-    clone(externalsDir, 'imgui', 'https://github.com/ocornut/imgui.git', 'f8c768760b0746aa7c1652397e2d9234c8502cb1')
-    clone(externalsDir, 'glfw', 'https://github.com/glfw/glfw.git', '3fa2360720eeba1964df3c0ecf4b5df8648a8e52')
+    libs = [
+        library('stb', 'https://github.com/nothings/stb.git', 'af1a5bc352164740c1cc1354942b1c6b72eacb8a'),
+        library('tinygltf', 'https://github.com/syoyo/tinygltf.git', 'aaf631c984c6e725573840c193ca2ff0ea216e7b'),
+        library('tinyply', 'https://github.com/ddiakopoulos/tinyply.git', 'e5d969413b8612de31bf96604c95bf294d406230'),
+        library('vulkan', 'https://github.com/KhronosGroup/Vulkan-Headers.git', '2b55157592bf4c639b76cc16d64acaef565cc4b5'),
+        library('imgui', 'https://github.com/ocornut/imgui.git', 'f8c768760b0746aa7c1652397e2d9234c8502cb1'),
+        library('glfw', 'https://github.com/glfw/glfw.git', '3fa2360720eeba1964df3c0ecf4b5df8648a8e52'),
+    ]
+    for lib in libs:
+        lib.clone(externalsDir)
 
     print("========== build glfw ==========")
-    osname = platform.platform()
-    if osname.startswith('Windows'):
-        subprocess.run( 'mkdir ' + externalsDir + '\\glfw\\build', shell=True)
-        subprocess.run( 'cmake -B ' + externalsDir + '\\glfw\\build -S ' + externalsDir + '\\glfw -DBUILD_SHARED_LIBS=ON', shell=True)
-        subprocess.run( 'cmake --build ' + externalsDir + '\\glfw\\build --config Release', shell=True)
-        subprocess.run( 'cmake --build ' + externalsDir + '\\glfw\\build --config Debug', shell=True)
-    elif osname.startswith('Linux'):
-        subprocess.run( 'mkdir ' + externalsDir + '/glfw/build', shell=True)
-        subprocess.run( 'cmake -B ' + externalsDir + '/glfw/build -S ' + externalsDir + '/glfw -DBUILD_SHARED_LIBS=ON', shell=True)
-        subprocess.run( 'make -C ' + externalsDir + '/glfw/build', shell=True)
+    glfwBuildDir = os.path.join(externalsDir, 'glfw', 'build')
+    subprocess.run( ' '.join(['mkdir', glfwBuildDir]), shell=True)
+    subprocess.run( ' '.join(['cmake', '-B', glfwBuildDir, '-S', os.path.join(externalsDir, 'glfw'), '-DBUILD_SHARED_LIBS=ON']), shell=True)
+    subprocess.run( ' '.join(['cmake', '--build', glfwBuildDir, '--config', 'Release']), shell=True)
+    subprocess.run( ' '.join(['cmake', '--build', glfwBuildDir, '--config', 'Debug']), shell=True)

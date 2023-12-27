96df46c342cfd25feb59a4660eadac33664201ac
kisskaDev <kirill_ii@mail.ru>
2023-12-28 00:24:39 +0100

    add script for compile cuda dir shaders

--- scripts/shaders_compile.sh
+++ scripts/shaders_compile.sh
@@ -2,3 +2,5 @@ full_path=$(realpath $0)
 dir_path=$(dirname $full_path)
 
 python3 $dir_path/shaders_compile.py -d $dir_path/../core/deferredGraphics/shaders -o $dir_path/../core/deferredGraphics/spv -c $dir_path/../dependences/libs/vulkan_tools/glslc
+
+python3 $dir_path/shaders_compile.py -d $dir_path/../core/cudaRayTracing/rayTracingGraphics/shaders -o $dir_path/../core/cudaRayTracing/rayTracingGraphics/spv -c $dir_path/../dependences/libs/vulkan_tools/glslc
